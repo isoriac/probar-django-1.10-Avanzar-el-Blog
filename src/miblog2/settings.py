7929d2c4b3d47c02a68880c341d4f3cefb38843a
Karlita K <Karlita K>
2016-09-21 10:27:46 +0200

    django crispy forms

--- src/miblog2/settings.py
+++ src/miblog2/settings.py
@@ -38,11 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #third part apps
+    'crispy_forms',
     'markdown_deux',
     'pagedown',
     #nuestras apps
     'posts',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
