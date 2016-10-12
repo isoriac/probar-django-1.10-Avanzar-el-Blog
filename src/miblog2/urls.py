3d1b424c9c3d0354d88a9e0c918bcb5e29b9b42a
Karlita K <Karlita K>
2016-10-12 08:39:06 +0200

    fadeToggle comment reply jQuery

--- src/miblog2/urls.py
+++ src/miblog2/urls.py
@@ -18,7 +18,7 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 
-from posts import views
+# from posts import views
 
 
 urlpatterns = [
