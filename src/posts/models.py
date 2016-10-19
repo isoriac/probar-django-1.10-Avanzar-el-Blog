f86995597e0bf1c0eefed21f2f1659f5f5a940fd
Karlita K <Karlita K>
2016-10-19 15:05:09 +0200

    tiempo lectura con django

--- src/posts/models.py
+++ src/posts/models.py
@@ -10,7 +10,10 @@ from django.utils.text import slugify
 
 from markdown_deux import markdown
 
+from .utils import get_read_time
+
 from comments.models import Comment
+
 
 # Create your models here.
 
@@ -37,6 +40,7 @@ class Post(models.Model):
 	contenido = models.TextField()
 	draft = models.BooleanField(default=False)
 	publish = models.DateField(auto_now=False, auto_now_add=False)
+	read_time = models.IntegerField(default=0)
 	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 	actualizado = models.DateTimeField(auto_now_add=False, auto_now=True)
 
@@ -86,5 +90,10 @@ def create_slug(instance, new_slug=None):
 def pre_save_post_receiver(sender, instance, *args, **kwargs): 
 	if not instance.slug:
 		instance.slug = create_slug(instance)
+
+	if instance.contenido:
+		html_string = instance.get_markdown()
+		read_time_var = get_read_time(html_string)
+		instance.read_time = read_time_var
 
 pre_save.connect(pre_save_post_receiver, sender=Post)
